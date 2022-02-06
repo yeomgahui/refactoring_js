a6d76168a8f390a2d1861391fc775b224701e3d4
gahui yeom <ykh4933@naver.com>
2022-02-06 12:43:22 +0900

    함수 인라인하기

---
+++ chapter6/inlineFunction.js
@@ -0,0 +1,26 @@
+function rating(aDriver) {
+    return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+function moreThanFiveLateDeliveries(aDriver) {
+    return aDriver.numberOfLateDeliveries > 5;
+}
+
+function rating(aDriver) {
+    return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
+}
+
+function reportLines(aCustomer) {
+    const lines = [];
+    gatherCustomerData(lines, aCustomer);
+    return lines;
+}
+function gatherCustomerData(out, aCustomer) {
+    out.push(["name", aCustomer.name]);
+    out.push(["location", aCustomer.location]);
+}
+function reportLines(aCustomer) {
+    const lines = [];
+    lines.push(["name", aCustomer.name]);
+    lines.push(["location", aCustomer.location]);
+    return lines;
+}
